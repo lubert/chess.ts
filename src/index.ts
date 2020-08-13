bce1ee40e8868855c42c10d6650575bb53567a3d
Lu Zhu <lu.zhu@instacart.com>
2020-08-13 09:16:00 -0700

    Export constants

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 export { Chess } from './chess'
 export { Color, FenComment, Move, Piece, PieceSymbol, Validation, State } from './types'
+export {
+    WHITE,
+    BLACK,
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING,
+    SQUARES,
+    FLAGS,
+    BITS,
+} from './constants'
