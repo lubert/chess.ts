1d1596beafe74ed2acbf25bde044d5f66fca7d89
Lu Zhu <misterzhu@gmail.com>
2021-07-07 19:19:58 -0700

    Fix make move

--- __tests__/chess.test.ts
+++ __tests__/chess.test.ts
@@ -1473,25 +1473,7 @@ describe('Manipulate Comments', function () {
   })
 })
 
-describe('Format Comments', function () {
-  it('wrap comments', function () {
-    const chess = new Chess()
-    chess.move('e4')
-    const e4 = chess.fen()
-    chess.setComment('good   move', e4)
-    chess.move('e5')
-    chess.setComment('classical response')
-    expect(chess.pgn()).toEqual('1. e4 {good   move} e5 {classical response}')
-    expect(chess.pgn()).toEqual(
-      ['1. e4 {good', 'move} e5', '{classical', 'response}'].join('\n')
-    )
-    expect(chess.pgn()).toEqual(
-      ['1.', 'e4', '{good', 'move}', 'e5', '{classical', 'response}'].join('\n')
-    )
-  })
-})
-
-describe('Load Comments', function () {
+describe('Load Comments', function() {
   const tests = [
     {
       name: 'bracket comments',
@@ -1565,89 +1547,82 @@ describe('Load Comments', function () {
 })
 
 describe('Make Move', function () {
-  const positions = [
+  interface MakeMoveExample {
+    name: string;
+    fen: string;
+    expect: boolean;
+    move: string;
+    next?: string;
+    captured?: string;
+  }
+
+  const examples: MakeMoveExample[] = [
     {
+      name: 'legal first move',
       fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-      legal: true,
+      expect: true,
       move: 'e4',
       next: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1',
     },
     {
+      name: 'illegal first move',
       fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-      legal: false,
+      expect: false,
       move: 'e5',
     },
     {
+      name: 'checkmate',
       fen: '7k/3R4/3p2Q1/6Q1/2N1N3/8/8/3R3K w - - 0 1',
-      legal: true,
+      expect: true,
       move: 'Rd8#',
       next: '3R3k/8/3p2Q1/6Q1/2N1N3/8/8/3R3K b - - 1 1',
     },
     {
+      name: 'white en passant',
       fen: 'rnbqkbnr/pp3ppp/2pp4/4pP2/4P3/8/PPPP2PP/RNBQKBNR w KQkq e6 0 1',
-      legal: true,
+      expect: true,
       move: 'fxe6',
       next: 'rnbqkbnr/pp3ppp/2ppP3/8/4P3/8/PPPP2PP/RNBQKBNR b KQkq - 0 1',
       captured: 'p',
     },
     {
+      name: 'black en passant',
       fen: 'rnbqkbnr/pppp2pp/8/4p3/4Pp2/2PP4/PP3PPP/RNBQKBNR b KQkq e3 0 1',
-      legal: true,
+      expect: true,
       move: 'fxe3',
       next: 'rnbqkbnr/pppp2pp/8/4p3/8/2PPp3/PP3PPP/RNBQKBNR w KQkq - 0 2',
       captured: 'p',
     },
 
-    // strict move parser
     {
+      name: 'correct disambiguation',
       fen: 'r2qkbnr/ppp2ppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R b KQkq - 3 7',
-      legal: true,
+      expect: true,
       next: 'r2qkb1r/ppp1nppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R w KQkq - 4 8',
       move: 'Ne7',
     },
-
-    // strict move parser should reject over disambiguation
     {
+      name: 'over disambiguation',
       fen: 'r2qkbnr/ppp2ppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R b KQkq - 3 7',
-      legal: false,
-      move: 'Nge7',
-    },
-
-    // sloppy move parser
-    {
-      fen: 'r2qkbnr/ppp2ppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R b KQkq - 3 7',
-      legal: true,
-      move: 'Nge7',
+      expect: true,
       next: 'r2qkb1r/ppp1nppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R w KQkq - 4 8',
-    },
-
-    // the sloppy parser should still accept correctly disambiguated moves
-    {
-      fen: 'r2qkbnr/ppp2ppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R b KQkq - 3 7',
-      legal: true,
-      move: 'Ne7',
-      next: 'r2qkb1r/ppp1nppp/2n5/1B2pQ2/4P3/8/PPP2PPP/RNB1K2R w KQkq - 4 8',
+      move: 'Nge7',
     },
   ]
 
-  positions.forEach(function (position) {
-    const chess = new Chess()
-    chess.load(position.fen)
-    it(
-      position.fen + ' (' + position.move + ' ' + position.legal + ')',
-      function () {
-        const result = chess.move(position.move)
-        if (position.legal) {
-          expect(
-            result &&
-              chess.fen() == position.next &&
-              result.captured == position.captured
-          ).toBe(true)
-        } else {
-          expect(result).toBeNull()
-        }
+  examples.forEach((example) => {
+    const chess = new Chess(example.fen)
+
+    it(example.name, () => {
+      const move = chess.move(example.move)
+      if (example.expect) {
+        expect(move).toBeDefined()
+        expect(chess.fen()).toEqual(example.next)
+        expect(move!.captured).toEqual(example.captured)
+      } else {
+        expect(move).toBeNull()
       }
-    )
+    })
   })
 })
 
