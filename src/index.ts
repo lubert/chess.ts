4ff53bcb27725dfe01e287b653d60aec615d31fc
Lu Zhu <misterzhu@gmail.com>
2021-07-11 16:21:47 -0700

    Export utils

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ export { Chess } from './chess'
 export { BoardState } from './models/BoardState'
 export { Color, Move, PartialMove, Piece, PieceSymbol, Validation } from './interfaces/types'
 export {
-    WHITE,
-    BLACK,
-    PAWN,
-    KNIGHT,
-    BISHOP,
-    ROOK,
-    QUEEN,
-    KING,
-    EMPTY,
-    SQUARES,
-    FLAGS,
-    BITS,
+  WHITE,
+  BLACK,
+  PAWN,
+  KNIGHT,
+  BISHOP,
+  ROOK,
+  QUEEN,
+  KING,
+  EMPTY,
+  SQUARES,
+  FLAGS,
+  BITS,
 } from './constants'
-export { rank, file, algebraic } from './utils'
+export * from './utils'
